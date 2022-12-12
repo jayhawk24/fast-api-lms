f8bba7c659bac1a933dfb3b20e273a8672616f4d
anubhav-sl <anubhav@steinnlabs.com>
2022-12-12 12:21:39 +0530

    add routers for courses and sections

---
+++ api/users.py
@@ -0,0 +1,30 @@
+import fastapi
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+router = fastapi.APIRouter()
+
+users = []
+
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str]
+
+
+@router.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@router.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return user
+
+
+@router.get("/users/{user_id}")
+async def get_user(id: int):
+    return {"user": [id]}
--- main.py
+++ main.py
@@ -1,32 +1,8 @@
-from typing import Optional, List
-from fastapi import FastAPI, Path, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
+from api import users, courses, sections
 
 app = FastAPI()
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    is_acitve: bool
-    bio: Optional[str]
-
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-
-@app.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return user
-
-
-@app.get("/users/{user_id}")
-async def get_user(
-    id: int = Path(..., description="The ID of the user to get", gt=0),
-    q: str = Query(None, max_length=5),
-):
-    return {"user": [id], "query": q}
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
